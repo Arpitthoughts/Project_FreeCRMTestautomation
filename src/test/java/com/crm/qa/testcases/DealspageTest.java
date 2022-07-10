2162b850c365decafa52ab53caca2bb6d6602606
Arpitthoughts <arjaria.arpit55@gmail.com>
2022-07-10 16:56:27 +0530

    added the DealsPageTest.java class please prepare test cases for deals page

---
+++ src/test/java/com/crm/qa/testcases/DealspageTest.java
@@ -0,0 +1,19 @@
+package com.crm.qa.testcases;
+
+import com.crm.qa.base.Testbase;
+
+public class DealspageTest extends Testbase {
+
+	
+	public DealspageTest() {
+		/*
+		 *This is a new class added to automate the 
+		 *test case for Deals page in free crm application 
+		 *Ajay please complete the test cases for Deals page and raise PR for review of code
+		 *after that i will merge your code in master branch
+		 * 
+		 * 
+		 */
+		
+	}
+}
